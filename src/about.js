f06c12a699277df7289b80b56de4f3ff3c3338ac
VolviX <20700808+VolviX@users.noreply.github.com>
2025-03-15 20:05:51 +0300

    Add missing commas

--- src/about.js
+++ src/about.js
@@ -10,4 +10,4 @@ export function about() {
     main.appendChild(desc);
 
     return main;
-}
+};
--- src/menu.js
+++ src/menu.js
@@ -10,4 +10,4 @@ export function menu() {
     main.appendChild(desc);
     
     return main;
-}
+};

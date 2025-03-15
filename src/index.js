67307c89eaf9afe875ddcbc36a699f7484fd72e9
VolviX <20700808+VolviX@users.noreply.github.com>
2025-03-15 18:42:26 +0300

    Add event listener to the main page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
 import "./style.css";
 import { home } from './home.js';
 
+// first load
 const defaultDiv = document.getElementById('content');
 const firstLoad = home();
 defaultDiv.appendChild(firstLoad);
+
+// listening the navigation buttons
+const nav = document.getElementById('nav');
+
+nav.addEventListener('click', (event) => {
+    if (event.target.tagName === 'BUTTON') {
+        const buttonId = event.target.id;
+
+        switch (buttonId) {
+            case 'home':
+              console.log('Home button was clicked!');
+              defaultDiv.textContent = '';
+              defaultDiv.appendChild(home());
+              break;
+            case 'menu':
+              console.log('Menu button was clicked!');
+              defaultDiv.textContent = '';
+              //defaultDiv.appendChild(menu());
+              break;
+            case 'about':
+              console.log('About button was clicked!');
+              defaultDiv.textContent = '';
+              //defaultDiv.appendChild(about());
+              break;
+        }
+    }
+});
